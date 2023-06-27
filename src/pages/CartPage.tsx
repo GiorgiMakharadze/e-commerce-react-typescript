05e06e7b2535789142f7b63c84f062d11bac8ac3
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-06-27 13:15:11 +0400

    pages initial setup & styling

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const CartPage = () => {
+  return <h4>cart page</h4>;
+};
+
+const Wrapper = styled.main`
+  .empty {
+    text-align: center;
+    h2 {
+      margin-bottom: 1rem;
+      text-transform: none;
+    }
+  }
+`;
+
+export default CartPage;
